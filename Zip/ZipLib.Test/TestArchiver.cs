353ae571c67635e319fb0b773a938d5c0ac17493
stovpyakas <stovpyak@rzdp.ru>
2016-08-24 15:16:36 +0800

    Версия 1) "Знакомство с GzipStream"

---
+++ Zip/ZipLib.Test/TestArchiver.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ZipLib.Test
+{
+    [TestClass]
+    public class TestArchiver
+    {
+        private void TestCompressDecompress(string sourceFileName, 
+            string compressedFileName, string decompressedNewFileName)
+        {
+            // prepare
+            if (File.Exists(compressedFileName))
+                File.Delete(compressedFileName);
+            if (File.Exists(decompressedNewFileName))
+                File.Delete(decompressedNewFileName);
+
+            // init
+            var archiver = new Archiver();
+
+            // execute
+            archiver.Compress(sourceFileName, compressedFileName);
+            Assert.IsTrue(File.Exists(compressedFileName), "compressed file not found");
+
+            archiver.Decompress(compressedFileName, decompressedNewFileName);
+            Assert.IsTrue(File.Exists(decompressedNewFileName), "decompressed file not found");
+        }
+
+
+        [TestMethod]
+        public void TestEmptyFile()
+        {
+            var sourceFileName = "..\\..\\..\\TestData\\EmptyFile.txt";
+            var compressedFileName = "..\\..\\..\\TestData\\EmptyFile.gz";
+            var decompressedNewFileName = "..\\..\\..\\TestData\\EmptyFileNew.txt";
+
+            TestCompressDecompress(sourceFileName, compressedFileName, decompressedNewFileName);
+        }
+
+        [TestMethod]
+        public void TestSimple()
+        {
+            var sourceFileName = "..\\..\\..\\TestData\\FileToCompress.txt";
+            var compressedFileName = "..\\..\\..\\TestData\\FileToCompress.gz";
+            var decompressedNewFileName = "..\\..\\..\\TestData\\FileToCompressNew.txt";
+
+            TestCompressDecompress(sourceFileName, compressedFileName, decompressedNewFileName);
+         }
+    }
+}
---
+++ Zip/ZipLib/Archiver.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace ZipLib
+{
+    public class Archiver
+    {
+        public void Compress(string sourceFileName, string targetFileName)
+        {
+            FileInfo fileToCompress = new FileInfo(sourceFileName);
+
+            using (FileStream originalFileStream = fileToCompress.OpenRead())
+            {
+                using (FileStream compressedFileStream = File.Create(targetFileName))
+                {
+                    using (GZipStream compressionStream = new GZipStream(compressedFileStream,
+                        CompressionMode.Compress))
+                    {
+                        originalFileStream.CopyTo(compressionStream);
+                    }
+                }
+            }
+        }
+
+        public void Decompress(string sourceFileName, string targetFileName)
+        {
+            var fileToDecompress = new FileInfo(sourceFileName);
+            using (FileStream originalFileStream = fileToDecompress.OpenRead())
+            {
+                using (FileStream decompressedFileStream = File.Create(targetFileName))
+                {
+                    using (GZipStream decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress))
+                    {
+                        decompressionStream.CopyTo(decompressedFileStream);
+                    }
+                }
+            }
+        }
+    }
+}
